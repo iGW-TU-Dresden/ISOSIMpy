9ed9b8cdbdc947ca839cf1e3f3780845b49544db
MaxGRudolph <77970220+MaxGRudolph@users.noreply.github.com>
2025-08-21 12:07:33 +0200

    fix docs

--- src/ISOSIMpy/model/__init__.py
+++ src/ISOSIMpy/model/__init__.py
@@ -2,6 +2,16 @@ from .model import Model
 from .solver import Solver
 from .units import EPMUnit, PMUnit, Unit
 
+# for docs
+# tell Sphinx that these objects "live" in their defining submodules,
+# even though we re-export them here
+# this prevents duplicate targets
+Model.__module__ = __name__ + ".model"
+Solver.__module__ = __name__ + ".solver"
+Unit.__module__ = __name__ + ".units"
+EPMUnit.__module__ = __name__ + ".units"
+PMUnit.__module__ = __name__ + ".units"
+
 __all__ = [
     "Model",
     "Solver",
